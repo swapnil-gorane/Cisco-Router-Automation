c5664916f0c40549eec7f10865c8f115410ff9dd
Swapnil Gorane <79601553+swapnil-gorane@users.noreply.github.com>
2022-08-17 18:32:23 +0530

    Add files via upload

---
+++ 01_3.10_telnet.py
@@ -0,0 +1,21 @@
+import getpass
+import sys
+import telnetlib
+
+HOST = "192.168.213.11"
+user = input("Enter your username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"term length 0\n")
+tn.write(b"show version\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
